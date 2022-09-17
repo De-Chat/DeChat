cf012ad0c79d7dff93eff18e031554145d3bee6c
chee-chyuan <angchee@live.com.my>
2022-09-17 16:32:55 +0800

    add modal

---
+++ packages/frontend/src/components/Modals/AddToContactModal.tsx
@@ -0,0 +1,70 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react'
+import React, { useState } from 'react'
+
+const AddToContactModal = (): JSX.Element => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const finalRef = React.useRef(null)
+
+  const [input, setInput] = useState<string>('')
+
+  const handleInputChange = (e: any) => setInput(e.target.value)
+
+  const isError = input === ''
+
+  return (
+    <>
+      <Box ref={finalRef} tabIndex={-1} aria-label="Focus moved to this box">
+        Some other content that'll receive focus on close.
+      </Box>
+
+      <Button mt={4} onClick={onOpen}>
+        Open Modal
+      </Button>
+      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add to Contacts</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl isInvalid={isError}>
+              <FormLabel>Nickname</FormLabel>
+              <Input
+                type="nickname"
+                value={input}
+                placeholder="Enter nickname.."
+                onChange={handleInputChange}
+              />
+              {isError && (
+                <FormErrorMessage>
+                  Enter a nickname for this contact
+                </FormErrorMessage>
+              )}
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variant="ghost">Add</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default AddToContactModal
