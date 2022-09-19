e4070ee3e956b628c23585e7337bf7ff0fe521dc
Ng Wei Han <47109095+weihan7742@users.noreply.github.com>
2022-09-19 23:22:14 +0800

    feat(UD): obtain avatar from UNS domain name

--- packages/frontend/src/components/Login.tsx
+++ packages/frontend/src/components/Login.tsx
@@ -18,96 +18,121 @@ import { SyntheticEvent, useCallback, useState } from 'react';
 import ThemeToggler from 'src/pages/ThemeToggler';
 import { useAccount } from 'wagmi';
 import Card from './Card';
+import useUnsAvatar from 'src/hooks/useUnsAvatar';
 
 const RegisterNickName = () => {
-  const [nickname, setNickname] = useState<string>('')
+  const [nickname, setNickname] = useState<string>('');
   const onInputChange = useCallback(
-    (e: React.FormEvent<HTMLInputElement>) => setNickname(e.currentTarget.value),
+    (e: React.FormEvent<HTMLInputElement>) =>
+      setNickname(e.currentTarget.value),
     [setNickname]
   );
 
   const submitNickname = (e: SyntheticEvent) => {
-    e.preventDefault()
-    console.log(nickname)
-  }
+    e.preventDefault();
+    console.log(nickname);
+  };
   return (
     <>
-      <Heading fontSize={'3xl'}>
-        It doesn't seem that you have a name?
-      </Heading>
+      <Heading fontSize={'3xl'}>It doesn't seem that you have a name?</Heading>
       <form onSubmit={submitNickname}>
         <FormControl id="name">
           <InputGroup>
-            <Input placeholder="ENS / Unstoppable Domains" onChange={onInputChange} />
-            <InputRightElement children={<IconButton disabled={!nickname} size='xs' aria-label='Register nickname' variant='ghost' as={BsAward} onClick={submitNickname} />} />
+            <Input
+              placeholder="ENS / Unstoppable Domains"
+              onChange={onInputChange}
+            />
+            <InputRightElement
+              children={
+                <IconButton
+                  disabled={!nickname}
+                  size="xs"
+                  aria-label="Register nickname"
+                  variant="ghost"
+                  as={BsAward}
+                  onClick={submitNickname}
+                />
+              }
+            />
           </InputGroup>
         </FormControl>
       </form>
     </>
-  )
-}
+  );
+};
 
 const Login = () => {
-  let { address } = useAccount()
+  let { address } = useAccount();
 
-  const resolvedName = '123'
+  const resolvedName = '123';
   return (
     <Flex minH={'100vh'} align={'center'} justify={'center'}>
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
         <Stack align={'center'}>
           <Heading fontSize={'4xl'}>Sign in to your account</Heading>
           <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
+            {/* to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️ */}
+            {useUnsAvatar('weihan37.wallet')}
           </Text>
         </Stack>
         <ThemeToggler />
         <Card rounded={'lg'} boxShadow={'lg'} p={8}>
-          <Stack spacing={3} align='center'>
-
-            {!address ?
-            <>
-              <ConnectButton.Custom>
-                {({ openConnectModal }) =>
-                  <Button onClick={openConnectModal}>
-                    Connect your wallet
-                  </Button>
-                }
-              </ConnectButton.Custom>
+          <Stack spacing={3} align="center">
+            {!address ? (
+              <>
+                <ConnectButton.Custom>
+                  {({ openConnectModal }) => (
+                    <Button onClick={openConnectModal}>
+                      Connect your wallet
+                    </Button>
+                  )}
+                </ConnectButton.Custom>
               </>
-              : <></>
-            }
+            ) : (
+              <></>
+            )}
 
-            {address ?
-              (resolvedName ?
+            {address ? (
+              resolvedName ? (
                 <>
                   <Heading fontSize={'3xl'}>
                     <Flex>
                       Welcome,
-                      <Text bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' fontSize='3xl' fontWeight='extrabold'>
+                      <Text
+                        bgGradient="linear(to-l, #7928CA, #FF0080)"
+                        bgClip="text"
+                        fontSize="3xl"
+                        fontWeight="extrabold"
+                      >
                         {resolvedName}
                       </Text>
                       &nbsp;!
                     </Flex>
                   </Heading>
                   <Text fontSize={'2xl'}>
-                    Waiting for signature&nbsp;<Spinner />
+                    Waiting for signature&nbsp;
+                    <Spinner />
                   </Text>
                   <ConnectButton.Custom>
-                    {({ openAccountModal }) =>
-                      <Text onClick={openAccountModal} fontSize={'md'} color={'blue.400'}>
+                    {({ openAccountModal }) => (
+                      <Text
+                        onClick={openAccountModal}
+                        fontSize={'md'}
+                        color={'blue.400'}
+                      >
                         Disconnect
                       </Text>
-                    }
+                    )}
                   </ConnectButton.Custom>
                 </>
-                :
+              ) : (
                 <>
-                <RegisterNickName />
-                </>)
-              : <></>
-
-            }
-
+                  <RegisterNickName />
+                </>
+              )
+            ) : (
+              <></>
+            )}
           </Stack>
         </Card>
       </Stack>
