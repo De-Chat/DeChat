cc84c4013be5b4ce8406310634b28991f8d4081f
Chong Yong Kin <ykchong45@gmail.com>
2022-09-19 08:33:52 +0000

    wip: add logic flow for Login

--- packages/frontend/src/pages/ThemeToggler.tsx
+++ packages/frontend/src/pages/ThemeToggler.tsx
@@ -1,13 +1,14 @@
-import { useColorMode } from '@chakra-ui/react';
-import { Button } from '@chakra-ui/react';
+import { IconButton, Icon, useColorMode } from '@chakra-ui/react';
+import { useMemo } from 'react';
+import {BsSun, BsMoonStars} from 'react-icons/bs'
 
 export default () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const buttonIcon = useMemo(() => colorMode == 'light' ? BsSun : BsMoonStars, [colorMode])
+
   return (
-    <header>
-      <Button onClick={toggleColorMode}>
+      <IconButton aria-label='Toggle theme' variant='outline' border='none' icon={<Icon as={buttonIcon} />} onClick={toggleColorMode}>
         Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-      </Button>
-    </header>
+      </IconButton>
   );
 };
