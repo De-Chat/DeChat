9156b36b9327916c56e8a85bcdd874d842763a51
Ng Wei Han <47109095+weihan7742@users.noreply.github.com>
2022-09-21 21:07:52 +0800

    feat: add utility hooks for unstoppable domain (#11)
    * feat(UD): resolves wallet address to UD domain
    
    * feat(UD): obtain avatar from UNS domain name

---
+++ packages/frontend/src/hooks/useUnsAvatar.ts
@@ -0,0 +1,17 @@
+import { Resolution } from '@unstoppabledomains/resolution';
+import { useState } from 'react';
+
+const useUnsAvatar = (domain : string) => {
+
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+  const resolution = new Resolution();
+
+  resolution
+    .tokenURIMetadata(domain)
+    .then((data) => {setAvatarUrl(data.image_url)})
+    .catch((err) => console.log('No avatar found for', domain));
+
+  return avatarUrl;
+}
+
+export default useUnsAvatar;
