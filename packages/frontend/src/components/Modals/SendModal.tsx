00e5dddcf28cc8bf4fd7382a94e474aee3e97c8d
Chong Yong Kin <ykchong45@gmail.com>
2022-09-22 05:46:33 +0000

    feat: add send NFT modal, update covalent API

--- packages/frontend/src/components/Modals/SendModal.tsx
+++ packages/frontend/src/components/Modals/SendModal.tsx
@@ -1,16 +1,17 @@
 import { useDeployments } from '@shared/useDeployments';
-import { chainId, useAccount, useContractRead, useNetwork, erc20ABI, useProvider, useBlockNumber, useContract } from 'wagmi';
+import { useContractRead, useNetwork, erc20ABI, useProvider, useBlockNumber, useContract, erc721ABI } from 'wagmi';
 import { Send__factory } from '@dechat/contracts/typechain-types';
 import useXmtp from '../../hooks/useXmtp';
-import React, { createContext, SyntheticEvent, useContext, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { ethers } from 'ethers';
 import BaseModal from './BaseModal';
-import { Button, Center, Checkbox, Container, Flex, FormControl, FormErrorMessage, HStack, IconButton, Input, ModalFooter, ModalHeader, Select, Spinner, Tab, TabList, TabPanel, TabPanels, Tabs, useDisclosure, VStack } from '@chakra-ui/react';
+import { Button, Center, Checkbox, Container, FormControl, FormErrorMessage, HStack, Input, ModalHeader, Select, Spinner, Tab, TabList, TabPanel, TabPanels, Tabs, useDisclosure, VStack } from '@chakra-ui/react';
 import { IoRocketOutline } from 'react-icons/io5'
-import { getERC20ByName, getERC20s } from '@deployments/tokenlist';
+import { getTokenBalancesForAddress, getNFTForAddress } from 'src/services/covalentService';
+import useAsyncEffect from 'use-async-effect';
 
 // helpers
-const approve = async (contract: ethers.Contract, address: string, amount: string, decimals: number) => {
+const approveErc20 = async (contract: ethers.Contract, address: string, amount: string, decimals: number) => {
   if (!contract) {
     console.warn('Contract not constructed!');
     return;
@@ -18,6 +19,21 @@ const approve = async (contract: ethers.Contract, address: string, amount: strin
   try {
     let tsx;
     tsx = await contract.approve(address, ethers.utils.parseUnits(amount, decimals));
+    const receipt = await tsx.wait();
+    console.log({ receipt });
+  } catch (e: any) {
+    console.error(e);
+  }
+};
+
+const approveErc721 = async (contract: ethers.Contract, address: string, tokenId: number) => {
+  if (!contract) {
+    console.warn('Contract not constructed!');
+    return;
+  }
+  try {
+    let tsx;
+    tsx = await contract.approve(address, tokenId);
     const receipt = await tsx.wait();
     console.log({ receipt });
   } catch (e: any) {
@@ -123,10 +139,11 @@ const SendToken = ({ disclosure }) => {
   const [error, setError] = useState(null)
   const handleFormChange = (e: React.FormEvent<HTMLInputElement>) => setForm({ ...form, [e.currentTarget.name]: e.currentTarget.value })
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const tokenName = e.target.value
+    const tokenAddr = e.target.value
     const chainId = chain?.id
-    console.log('test select: ', chain?.id, e.target.value, getERC20ByName(chainId, tokenName))
-    setForm({ ...form, token: getERC20ByName(chainId, tokenName) })
+    console.log('test select: ', chainId, tokenAddr, getTokenByAddress(tokenAddr))
+    setForm({ ...form, token: getTokenByAddress(tokenAddr) })
+    console.log('test select token: ', e.target.value)
   }
   const handleCheckboxChange = (e: React.FormEvent<HTMLInputElement>) => setForm({ ...form, [e.currentTarget.name]: e.currentTarget.checked})
   // TODO: validation
@@ -177,7 +194,7 @@ const SendToken = ({ disclosure }) => {
       erc20ABI, 
       signer)
     console.log('test erc20: ', token.address, to, amount)
-    await approve(tokenContract, contracts?.Send.address, amount, token.decimals)
+    await approveErc20(tokenContract, contracts?.Send.address, amount, token.decimals)
     setBusy(false)
   }
 
@@ -203,7 +220,17 @@ const SendToken = ({ disclosure }) => {
 
   // tokenlist
   const { chain } = useNetwork()
-  const tokenlist = useMemo(() => getERC20s(chain?.id) || [], [chain?.id])
+  // const tokenlist = useMemo(() => getERC20s(chain?.id) || [], [chain?.id])
+  const [tokenlist, setTokenlist] = useState([])
+  useAsyncEffect(async () => {
+    if (chain?.id) {
+      const tokenlistForChain = await getTokenBalancesForAddress(chain?.id, address)
+      setTokenlist(tokenlistForChain?.tokens)
+    }
+  }, [chain?.id, address])
+  const getTokenByAddress = useCallback((tokenAddr) => tokenlist.find(t => t.address == tokenAddr), [tokenlist])
+
+  console.log('test tokenlist: ', tokenlist)
 
   return (
     <Container centerContent>
@@ -212,13 +239,149 @@ const SendToken = ({ disclosure }) => {
           <Input placeholder='Receiver' name='to' onChange={handleFormChange} />
           <HStack gap={2}>
             <Select placeholder='Token' name='token' onChange={handleSelectChange}>
-              {tokenlist.map((token: any) =>
-                <option key={token.name} value={token.name}>{token.name.toUpperCase()}</option>
+              {tokenlist?.map((token: any) =>
+                <option key={token.address} value={token.address}>{token.symbol}</option>
               )}
             </Select>
             <Input placeholder='Amount' name='amount' type='number' onChange={handleFormChange} />
           </HStack>
           <Checkbox name='lend' onChange={handleCheckboxChange}>Lend to receiver</Checkbox>
+          {error && (
+            <FormErrorMessage>
+              {error}
+            </FormErrorMessage>
+          )}
+        </VStack>
+      </FormControl>
+
+      <Center mt={5} >
+        {busy ? <Spinner />
+          : (approved ? <Button onClick={onSend}>Send</Button>
+            : <Button onClick={onApprove}>Approve</Button>)
+        }
+      </Center>
+    </Container>
+  )
+}
+
+// send NFT
+const SendNFT = ({ disclosure }) => {
+  const [busy, setBusy] = useState(false)
+  useEffect(() => {
+    if (!disclosure.isOpen) {
+      setBusy(false)
+      setForm({})
+    }
+  }, [disclosure.isOpen])
+
+  // form update 
+  const [form, setForm] = useState<any>({})
+  const [error, setError] = useState(null)
+  const handleFormChange = (e: React.FormEvent<HTMLInputElement>) => setForm({ ...form, [e.currentTarget.name]: e.currentTarget.value })
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log('test select: ', chain?.id, e.target.name, e.target.value)
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+  // TODO: validation
+  const validateForm = () => {
+    if (!form['to'] || !form['contract'] || !form['tokenId']) {
+      setError('Transaction info is incomplete')
+      return false
+    }
+    return true
+  }
+  console.log('test form: ', form)
+
+  // contract and stuff
+  const { wallet: signer, walletAddress: address } = useXmtp();
+  console.log('test current addr: ', address, signer);
+  const { contracts } = useDeployments();
+  // Approve contract and read / write function
+  const { data: getApproved, isError, isLoading } = useContractRead({
+    addressOrName: form.contract,
+    contractInterface: erc721ABI,
+    functionName: 'getApproved',
+    args: [form.tokenId],
+    watch: true
+  })
+  const approved = useMemo(() => {
+    try {
+      return getApproved == contracts?.Send.address
+    } catch (e) {
+      return false
+    }
+  }, [getApproved, contracts])
+  // const tokenContract = useContract({
+  //   addressOrName: form.token?.address, 
+  //   contractInterface: erc20ABI, 
+  //   signerOrProvider: signer
+  // })
+  const onApprove = async () => {
+    if (!validateForm())
+      return
+
+    setBusy(true)
+    const nftContract = new ethers.Contract(
+      form.contract, 
+      erc721ABI, 
+      signer)
+    console.log('test erc721: ', )
+    await approveErc721(nftContract, contracts?.Send.address, form?.tokenId)
+    setBusy(false)
+  }
+
+  // ERC721 contract and write function
+  const sendContract = useMemo(() => {
+    if (!signer || !contracts) return null;
+    return Send__factory.connect(contracts.Send.address, signer);
+  }, [signer, contracts]);
+
+  const onSend = async () => {
+    if (!validateForm())
+      return
+
+    setBusy(true)
+    let { to, contract, tokenId } = form
+    await sendERC721(sendContract, contract, to, tokenId)
+
+    // TODO: write to tableland if this is a lend
+
+    disclosure.onClose()
+    setBusy(false)
+  }
+
+  // tokenlist
+  const { chain } = useNetwork()
+  const [nfts, setNfts] = useState([])
+  useAsyncEffect(async () => {
+    if (chain?.id) {
+      const nftsForChain = await getNFTForAddress(chain?.id, address)
+      setNfts(nftsForChain)
+    }
+  }, [chain?.id, address])
+  const tokenIds = useMemo(() => {
+    if (form.contract && nfts.length)
+      return nfts.find(n => n.address == form.contract).data
+    return []
+  }, [nfts, form.contract])
+
+  console.log('test nfts and tokenid: ', nfts, tokenIds)
+
+  return (
+    <Container centerContent>
+      <FormControl mt={3} mb={3} onSubmit={onSend}>
+        <VStack spacing={4}>
+          <Input placeholder='Receiver' name='to' onChange={handleFormChange} />
+            <Select placeholder='Contract' name='contract' onChange={handleSelectChange}>
+              {nfts.map((nft: any) =>
+                <option key={nft.name} value={nft.address}>{nft.name}</option>
+              )}
+            </Select>
+            <Select placeholder='Token ID' name='tokenId' onChange={handleSelectChange}>
+              {tokenIds.map((tokenId: any) =>
+                <option key={tokenId.name} value={tokenId.id}>{tokenId.name}</option>
+              )}
+            </Select>
           {error && (
             <FormErrorMessage>
               {error}
@@ -244,7 +407,7 @@ const tabs = [
   },
   {
     name: 'NFT',
-    component: (props) => <>2</>
+    component: (props) => <SendNFT {...props} />
   },
   {
     name: 'STREAM',
@@ -252,7 +415,6 @@ const tabs = [
   },
 ]
 
-// const BusyContext = createContext()
 const SendModal = () => {
   const disclosure = useDisclosure()
 
--- packages/frontend/src/services/covalentService.ts
+++ packages/frontend/src/services/covalentService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-
+// sample token return
 // {
 //     "contract_decimals": 18,
 //     "contract_name": "Dank Token",
@@ -43,7 +43,8 @@ export const getTokenBalancesForAddress = async (chain_id: number, address: stri
     ).then(
         res => {
             const data = res.data.data
-            let nonZeroTokens = data.items.filter(token => token.quote > 0)
+            // let nonZeroTokens = data.items.filter(token => token.quote > 0)
+            let nonZeroTokens = data.items
             let tokens: ITokenBalance[] = nonZeroTokens.map(token => ({
                 address: token.contract_address,
                 symbol: token.contract_ticker_symbol,
@@ -62,4 +63,76 @@ export const getTokenBalancesForAddress = async (chain_id: number, address: stri
             console.log(returnData)
             return returnData
         }).catch(e => console.warn("Covalent service encouters error: ", e))
+}
+
+// sample NFT return
+// {
+//     "contract_decimals": 0,
+//     "contract_name": "Perry Cooper",
+//     "contract_ticker_symbol": "PERRYCOOPER",
+//     "contract_address": "0x591fb4438a0fb02c689105b1d29aa172be6db049",
+//     "supports_erc": [
+//         "erc20",
+//         "erc721"
+//     ],
+//     "logo_url": "https://logos.covalenthq.com/tokens/1/0x591fb4438a0fb02c689105b1d29aa172be6db049.png",
+//     "last_transferred_at": "2020-06-26T00:00:42Z",
+//     "native_token": false,
+//     "type": "nft",
+//     "balance": "1",
+//     "balance_24h": null,
+//     "quote_rate": 0,
+//     "quote_rate_24h": null,
+//     "quote": 0,
+//     "quote_24h": null,
+//     "nft_data": [
+//         {
+//             "token_id": "2100060058",
+//             "token_balance": null,
+//             "token_url": "https://api.niftygateway.com/perrycooper/2100060058",
+//             "supports_erc": [
+//                 "erc20",
+//                 "erc721"
+//             ],
+//             "token_price_wei": null,
+//             "token_quote_rate_eth": null,
+//             "original_owner": "0xe052113bd7d7700d623414a0a4585bcae754e9d5",
+//             "external_data": {
+//                 "name": "Trouble Brewing #58/100",
+//                 "description": "Trouble Brewing by Perry Cooper",
+//                 "image": "https://res.cloudinary.com/nifty-gateway/video/upload/v1593022381/PerryCooper/niftygateway_teapot4_culkeh.png",
+//                 "image_256": "https://image-proxy.svc.prod.covalenthq.com/cdn-cgi/image/width=256,fit/https://res.cloudinary.com/nifty-gateway/video/upload/v1593022381/PerryCooper/niftygateway_teapot4_culkeh.png",
+//                 "image_512": "https://image-proxy.svc.prod.covalenthq.com/cdn-cgi/image/width=512,fit/https://res.cloudinary.com/nifty-gateway/video/upload/v1593022381/PerryCooper/niftygateway_teapot4_culkeh.png",
+//                 "image_1024": "https://image-proxy.svc.prod.covalenthq.com/cdn-cgi/image/width=1024,fit/https://res.cloudinary.com/nifty-gateway/video/upload/v1593022381/PerryCooper/niftygateway_teapot4_culkeh.png",
+//                 "animation_url": null,
+//                 "external_url": "https://niftygateway.com/#/",
+//                 "attributes": null,
+//                 "owner": "0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de"
+//             },
+//             "owner": "0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de",
+//             "owner_address": null,
+//             "burned": null
+//         }
+//     ]
+// }
+export const getNFTForAddress = async (chain_id: number, address: string) => {
+    return await axios.get(
+        `https://api.covalenthq.com/v1/${chain_id}/address/${address}/balances_v2/?&nft=true&key=${process.env.NEXT_PUBLIC_COVALENT_API}`
+    ).then(
+        res => {
+            const data = res.data.data
+            let nfts = data.items.filter(token => token.type == 'nft')
+            const returnData = nfts.map(nft => ({
+                address: nft.contract_address,
+                name: nft.contract_name,
+                data: nft.nft_data.map(id => ({
+                    id: id.token_id,
+                    image: id.external_data?.image,
+                    name: id.external_data?.name
+                }))
+            }))
+
+            console.log(returnData)
+            return returnData
+        }).catch(e => console.warn("Covalent NFT service encouters error: ", e))
 }
